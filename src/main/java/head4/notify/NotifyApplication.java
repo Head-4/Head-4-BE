60f399750341c3937d9d0f6be61241bac29850bb
bak3839 <bak3839@naver.com>
2025-03-12 15:11:37 +0900

    feat: TimeZone Asia/Seoul 설정

--- src/main/java/head4/notify/NotifyApplication.java
+++ src/main/java/head4/notify/NotifyApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @OpenAPIDefinition(servers = {
 		@Server(url = "http://localhost:8080", description = "로컬 URL"),
 		@Server(url = "https://server.univ-on.com", description = "서버 URL")
@@ -15,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class NotifyApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(NotifyApplication.class, args);
 	}
 
